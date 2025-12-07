a93b3593949803bcb9042dcb68f42248742303d7
HyeokE <jhjeong00@gmail.com>
2025-12-08 01:00:36 +0900

    chore: remove GitHub Pages build script and update deployment workflow to Firebase Hosting

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.VITE_GITHUB_PAGES ? '/party/' : '/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
